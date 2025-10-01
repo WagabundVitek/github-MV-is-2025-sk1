1e5467046eee80fb9f8268f9cbf02b05e5ce3430
Martin <marvit777@seznam.cz>
2025-10-01 16:57:27 +0200

    #3 základ programu - výpis-řady

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,26 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("------------------------------------");
+    Console.WriteLine("----------Vypis rady cisel----------");
+    Console.WriteLine("------------------------------------");
+    Console.WriteLine("-------------Bomboclatos------------");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte prvni cislo rady: ");
+    //int prvni = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu - řešený správně
+    Console.Write("Zadejte prvni cislo rady (cele cislo)");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste cele cislo. Zadejte prvrni cislo rady znovu. ");
+}
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakovani stisknete klavesu a.");
+    again = Console.ReadLine();
+}
