b1a8ce514312c836f2a86ba81f014abf885167a1
Martin.Vitek <martin.vitek@tul.cz>
2025-10-14 14:32:40 +0200

    #4 soucet + soucin

--- IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
+++ IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e5467046eee80fb9f8268f9cbf02b05e5ce3430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c71ed182c023c7def9c4934c1c4030950255284f")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- IS-Programy/program002-soucet-cifer/Program.cs
+++ IS-Programy/program002-soucet-cifer/Program.cs
@@ -1,20 +1,46 @@
 ﻿int x = 1;
+int soucet = 0;
+int soucin = 1;
 while (x == 1)
 {
     Console.WriteLine("Zadejte cislo:");
-    string cislo = Console.ReadLine();
-    if (int.TryParse(cislo, out int vstup))
+    string? cislo = Console.ReadLine();
+    if (cislo == null)
+{
+    Console.WriteLine("Nebyl zadán žádný vstup!");
+    continue;
+}
+    else if (int.TryParse(cislo, out int vstup))
     {
         for (int i = 0; i < cislo.Length; i++)
         {
             int cislice = cislo[i] - '0';
             Console.WriteLine($"Pozice {i + 1}: {cislice}");
+            soucet += cislice;
+            soucin *= cislice;
+        }
+        Console.WriteLine($"Soucet cifer je: {soucet}");
+        Console.WriteLine($"Soucin cifer je: {soucin}");
+            soucet = 0;
+            soucin = 1;
+        Console.WriteLine("Chcete pokracovat? (ano/ne)");
+        string? pokracovat = Console.ReadLine();
+
+        if (pokracovat == null || pokracovat.ToLower() != "ano")
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            continue;
         }
     }
 
     else
     {
-        Console.WriteLine("To neni cislo! Pro opakovani stiskněte enter.");
-        Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("To neni cislo! Zkuste to znovu.");
+        continue;
     }
          }
