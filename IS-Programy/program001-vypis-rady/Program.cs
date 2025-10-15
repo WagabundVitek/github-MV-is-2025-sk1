b4f50dd783ff0b5edaf15a9cb052d5bb71d5978a
Martin <marvit777@seznam.cz>
2025-10-15 09:31:10 +0200

    #3 výpis proměnných

--- IS-Programy/program001-vypis-rady/Program.cs
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -18,6 +18,7 @@ while (again == "a")
     while (!int.TryParse(Console.ReadLine(), out first))
     {
         Console.Write("Nezadali jste cele cislo. Zadejte prvrni cislo rady znovu. ");
+        Console.WriteLine();
     }
 
     Console.Write("Zadejte posledni cislo rady (cele cislo)");
@@ -25,6 +26,7 @@ while (again == "a")
     while (!int.TryParse(Console.ReadLine(), out last))
     {
         Console.Write("Nezadali jste cele cislo. Zadejte posledni cislo rady znovu. ");
+        Console.WriteLine();
     }
 
     Console.Write("Zadejte diferenci (cele cislo)");
@@ -32,7 +34,19 @@ while (again == "a")
     while (!int.TryParse(Console.ReadLine(), out step))
     {
         Console.Write("Nezadali jste cele cislo. Zadejte diferenci znovu. ");
+        Console.WriteLine();
     }
+
+    //vypis vstupnich hodnot
+    Console.WriteLine();
+    Console.WriteLine("############################");
+    Console.WriteLine("Zadaji jste tyto hodnoty:");
+    Console.WriteLine($"Prvni cislo rady:{first}");
+    Console.WriteLine($"Posledni cislo rady:{last}");
+    Console.WriteLine($"Diference:{step}");
+    Console.WriteLine($"###########################");
+    Console.WriteLine("Prvni cislo: {0}, Posledni cislo: {1}, Diference: {2}", first, last, step);
+
 
     Console.WriteLine();
     Console.WriteLine("Pro opakovani stisknete klavesu a.");
