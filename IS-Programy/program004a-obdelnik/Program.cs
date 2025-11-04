78e4af242684c0e0c7175dd993c6e7dec0422e7e
Martin.Vitek <martin.vitek@tul.cz>
2025-11-04 12:25:08 +0100

    #5 Vykreslení obdélníku a trojúhelníku

---
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Program pro vykreslení obdélníku");
+
+Console.Write("Zadejte šířku obdélníku: ");
+int width = int.Parse(Console.ReadLine());
+
+Console.Write("Zadejte výšku obdélníku: ");
+int height = int.Parse(Console.ReadLine());
+
+Console.Write("┌"); //  Vypíše levý horní roh
+
+for (int i = 0; i < width; i++)
+{
+    
+    Console.Write("─");
+}
+Console.WriteLine("┐"); //  Vypíše pravý horní roh
+
+for (int i = 0; i < height; i++)
+{
+    Console.Write("│"); // výška levá
+    for (int j = 0; j < width; j++) //  vyplní střed mezerami podle zadané šířky
+    {
+        Console.Write(" ");
+    }
+    Console.WriteLine("│"); // Zakončí řádek svislou čarou
+}
+
+Console.Write("└"); //  levý dolní roh
+for (int i = 0; i < width; i++)// Opakuje vodorovné čáry podle zadané šířky
+{
+    Console.Write("─");
+}
+Console.WriteLine("┘"); // pravý dolní roh
---
+++ IS-Programy/program004b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,64 @@
+﻿// Vypíše úvodní zprávu programu
+Console.WriteLine("Program pro vykreslení pravoúhlého trojúhelníku");
+
+int height = 0;
+bool isValidInput = false;
+
+
+while (!isValidInput)
+{
+    Console.Write("Zadejte výšku trojúhelníku: "); // Požádá uživatele o zadání výšky trojúhelníku
+    string? input = Console.ReadLine();
+
+    // Kontrola prázdného vstupu
+    if (string.IsNullOrEmpty(input))
+    {
+        Console.WriteLine("Neplatný vstup! Zadejte prosím číslo.");
+        continue;
+    }
+
+
+    if (int.TryParse(input, out height)) // Pokusí se převést vstup na číslo
+    {
+        if (height > 0)
+        {
+            isValidInput = true;
+        }
+        else
+        {
+            Console.WriteLine("Výška musí být větší než 0!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Neplatný vstup! Zadejte prosím celé číslo.");
+    }
+}
+
+Console.Clear();
+Console.WriteLine($"Pravoúhlý trojúhelník s výškou: {height}"); 
+Console.Write("┌"); // Vypíše levý horní roh
+Console.WriteLine("┐");// Vykreslí horní vodorovnou čáru ┌┐
+
+for (int i = 0; i < height - 2; i++)
+{
+    Console.Write("│"); // Levá svislá čára
+
+    for (int j = 0; j < i; j++) // Mezery před šikmou čárou (obsah)
+    {
+        Console.Write(" ");
+    }
+    
+    // Šikmá čára
+    Console.Write("\\");
+    
+    Console.WriteLine();
+}
+
+
+Console.Write("└"); // Vykreslení levého spodního rohu
+for (int i = 0; i < height - 2; i++) // Vykreslí spodní čáru
+{
+    Console.Write("─"); // Spodní čáta
+}
+Console.WriteLine("┘"); // Vykreslí pravý spodní roh
